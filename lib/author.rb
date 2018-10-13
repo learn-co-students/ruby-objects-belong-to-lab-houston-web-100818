6420929400bc34fd43ba9bbd53b6c12653b2654c
ngouti <gouti.naama@gmail.com>
2018-10-12 21:20:49 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,12 @@
+class Author
+
+    attr_accessor :name
+
+    def initialize
+        self.name = name
+    end
+
+
+
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+  class Post
+
+    attr_accessor :title, :author
+    # attr_reader :author
+
+    def initialize
+        self.title = title
+    end
+
+
+  end
