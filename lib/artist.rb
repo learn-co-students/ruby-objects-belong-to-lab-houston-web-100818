a80c0a641cafa13cd8a7d801f3b28a3d73779037
jordan-laird <jordanmack.laird@gmail.com>
2018-10-15 15:41:38 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+
+require_relative "../lib/song.rb"
+
+
+class Artist
+    attr_accessor :name
+
+    # def initialize(name)
+    #     @name = name
+    # end
+
+end
+
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/artist.rb"
+
+class Song
+    attr_accessor :title, :artist
+
+    # def initialize(title)
+    #     @title = title
+    # end
+
+    
+end
